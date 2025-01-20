77f7acf56c5943a5832e6bf79cb36a3a7404e594
deomrd <deomrd1@gmail.com>
2025-01-20 15:39:42 +0200

    update roots

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -6,12 +6,12 @@ import { ajouterAchat } from '../controllers/achatsController';
 
 export const router = Router();
 
-router.post('/signup', signup); // route pour inscription
-router.post('/login', login); // route pour login
-router.post('/fournisseur', ajouterFournisseur); // route pour ajouter un fournisseur
-router.get('/fournisseurs', listerFournisseurs); // route pour afficher tous les fournisseurs
-router.post('/produit', ajouterProduit); // route pour ajouter un produit
-router.get('/produits', listerProduits); // route pour afficher tous les produits
-router.get('/produits/:idProduit', afficherUnProduit); // route pour afficher un seul produit
-router.post('/achats', ajouterAchat); // route pour ravitailler le stock
+router.post('/gesapp/signup', signup); // route pour inscription
+router.post('/gesapp/login', login); // route pour login
+router.post('/gesapp/fournisseur', ajouterFournisseur); // route pour ajouter un fournisseur
+router.get('/gesapp/fournisseurs', listerFournisseurs); // route pour afficher tous les fournisseurs
+router.post('/gesapp/produit', ajouterProduit); // route pour ajouter un produit
+router.get('/gesapp/produits', listerProduits); // route pour afficher tous les produits
+router.get('/gesapp/produits/:idProduit', afficherUnProduit); // route pour afficher un seul produit
+router.post('/gesapp/achats', ajouterAchat); // route pour ravitailler le stock
 
