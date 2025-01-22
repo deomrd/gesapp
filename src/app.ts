e27ea0f5be89a1142b8ce9fce0d833f54168b688
deomrd <deomrd1@gmail.com>
2025-01-22 13:06:54 +0200

    update middelare de cors pour autoriser le frontend

--- src/app.ts
+++ src/app.ts
@@ -9,10 +9,17 @@ const prisma = new PrismaClient();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 app.get('/gesapp', (req: Request, res: Response) => {
   res.send('Le serveur est bien demarré');
 });
+
 
 
 app.use(router);
